6e86f5417e4e4fc9e996dc0415d86f4b0d2b8e6e
Jesse Glick <jglick@cloudbees.com>
2014-08-06 14:36:34 -0400

    JUnit 4.

--- src/test/java/hudson/tasks/javadoc/JavadocArchiverTest.java
+++ src/test/java/hudson/tasks/javadoc/JavadocArchiverTest.java
@@ -2,21 +2,23 @@ package hudson.tasks.javadoc;
 
 import hudson.model.FreeStyleProject;
 import hudson.tasks.JavadocArchiver;
-import org.jvnet.hudson.test.HudsonTestCase;
+import org.junit.Rule;
+import org.junit.Test;
+import org.jvnet.hudson.test.JenkinsRule;
 
-/**
- * @author Kohsuke Kawaguchi
- */
-public class JavadocArchiverTest extends HudsonTestCase {
-    public void testConfigRoundtrip() throws Exception {
+public class JavadocArchiverTest {
+
+    @Rule public JenkinsRule r = new JenkinsRule();
+
+    @Test public void configRoundtrip() throws Exception {
         verify(new JavadocArchiver("foo", true));
         verify(new JavadocArchiver(".", false));
     }
 
     private void verify(JavadocArchiver before) throws Exception {
-        FreeStyleProject p = createFreeStyleProject();
+        FreeStyleProject p = r.createFreeStyleProject();
         p.getPublishersList().add(before);
-        configRoundtrip(p);
-        assertEqualDataBoundBeans(before, p.getPublishersList().get(JavadocArchiver.class));
+        r.configRoundtrip(p);
+        r.assertEqualDataBoundBeans(before, p.getPublishersList().get(JavadocArchiver.class));
     }
 }
